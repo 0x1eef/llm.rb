54ae1ee5847964e0484c47b5375169429d9c567a
robert <robert@r.uby.dev>
2026-09-05 07:20:35 +0000

    tools(git): add LLM::Tool::Git.actions
    This new method defines what git actions,
    or subcommands it can perform. The array
    is splat into Enum.[].

--- lib/llm/tools/git.rb
+++ lib/llm/tools/git.rb
@@ -7,10 +7,19 @@ class LLM::Tool
   class Git < self
     require_relative "exec"
 
+    ##
+    # @return [Array<String>]
+    #  The git actions that can be performed by this tool.
+    def self.actions
+      ["log", "diff",
+       "commit", "checkout",
+       "branch", "show"]
+    end
+
     name "git"
     description "Perform an action with git\n" \
                 "This command (git) is spawned without a shell"
-    parameter :action, Enum["log", "diff", "commit", "checkout", "branch", "show"], "the git operation to perform"
+    parameter :action, Enum[*actions], "the git operation to perform"
     parameter :arguments, Array[String], "one or more arguments for the git action"
     parameter :timeout, Integer, "the maximum time to allow the command to run"
     required %i[action]
